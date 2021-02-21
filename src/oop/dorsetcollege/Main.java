ac6cf3aeac9c8a4c23b8f44a1f869b645c61d925
taivieras <taiveiras@gmail.com>
2021-02-21 21:42:38 +0000

    First Commit

---
+++ src/oop/dorsetcollege/Main.java
@@ -0,0 +1,19 @@
+package oop.dorsetcollege;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        LabelFrame labelFrame = new LabelFrame();
+
+        labelFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        labelFrame.setSize(350, 180);
+
+        labelFrame.setVisible(true);
+
+    }
+}
